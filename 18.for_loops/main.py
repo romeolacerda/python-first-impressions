8973e3a9a463b9d2b52bc88ea532faece9a8c980
Romeo <romeolacerdafarias@gmail.com>
2025-11-29 20:18:56 -0300

    18 for loops

---
+++ 18.for_loops/main.py
@@ -0,0 +1,7 @@
+# for loops = execute a block of code a fixed number of times. You can interate over a range, string, sequence, etc
+
+for x in range(1, 21):
+    if x == 13:
+        break
+    else:
+        print(x)
