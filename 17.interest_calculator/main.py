fd35992c1b07dcdf8c8b7bd0ad04c0d42db44e1e
Romeo <romeolacerdafarias@gmail.com>
2025-11-29 11:37:05 -0300

    17 interest calculator

---
+++ 17.interest_calculator/main.py
@@ -0,0 +1,29 @@
+
+principle = 0
+rate = 0
+time = 0
+
+while True:
+    principle = float(input("Enter the principle amout: "))
+    if principle < 0:
+        print("principle must be greater than zero")
+    else:
+        break
+        
+while True:
+    rate = float(input("Enter the interest rate: "))
+    if rate < 0:
+        print("Interest rate must be greater than zero")
+    else:
+        break
+        
+while True:
+    time = int(input("Enter the time in years: "))
+    if time < 0:
+        print("Time must be greater than zero")
+    else:
+        break
+        
+total = principle * pow((1 + rate / 100), time)
+
+print(f"Balance after {time} year/s: ${total:.2f}")
